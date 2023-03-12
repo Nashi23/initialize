684d094a24649dcc8c58553a0a8cf0241a15d866
Nashi23 <neko.dev@foxmail.com>
2023-03-13 04:56:33 +0800

    Optimize formatting

--- shell/iptables.sh
+++ shell/iptables.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # reset the default policies in the filter table.
 iptables -P INPUT ACCEPT
 iptables -P FORWARD ACCEPT
@@ -21,14 +23,15 @@ iptables -t mangle -F
 iptables -X
 iptables -t nat -X
 iptables -t mangle -X
-# All rules have been deleted
 
 # Allow ssh port
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
+
 # Allow established connections
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
 # Allow local and lan access to all ports
 iptables -A INPUT -p tcp -j ACCEPT -s 127.0.0.1
 #iptables -A INPUT -s 10.0.0.0/8 -j ACCEPT
